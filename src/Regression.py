b870fa1df2f9e14b6e5a5569aa26ecbab4d7b7cc
barearojo <barearojojuan@gmail.com>
2023-12-21 13:37:57 +0100

    Estructura hecha del git

--- src/Regression.py
+++ src/Regression.py
@@ -6,3 +6,10 @@ Created on Thu Dec 21 13:29:04 2023
 @author: juan
 """
 
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+train = pd.read_csv("../data/train.csv", na_values="NaN") # Definimos na_values para identificar bien los valores perdidos
+print(train.columns)
