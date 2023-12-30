380b73b48196626c2dec7617564cbf4e896d1a64
barearojo <barearojojuan@gmail.com>
2023-12-30 20:55:28 +0100

    entrega 6 XGBoost

--- src/Regression.py
+++ src/Regression.py
@@ -5,8 +5,7 @@ Created on Thu Dec 21 13:29:04 2023
 
 @author: juan
 """
-
-import sys
+import sklearn
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
@@ -15,6 +14,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.impute import SimpleImputer
+import xgboost as xgb
 
 #leemos los datos tanto del entrenamiento como del test y marcamos lo perdidos como NaN
 train = pd.read_csv("../data/train.csv", na_values="NaN") # Definimos na_values para identificar bien los valores perdidos
@@ -42,7 +42,7 @@ test[col_cat] = imputer_cat.transform(test[col_cat])
 #Ahora reemplazo los valores numéricos por prediccion knni
 col_num = list(train.select_dtypes(include=np.number).columns)
 col_num.remove('SalePrice')
-imputer_num = KNNImputer(n_neighbors=10)
+imputer_num = KNNImputer(n_neighbors=5)
 imputer_num.fit(input_all[col_num])
 train[col_num] = imputer_num.transform(train[col_num])
 test[col_num] = imputer_num.transform(test[col_num])
@@ -67,11 +67,14 @@ if 'Id' in test_l:
 X_test = test_l
 
 
-model = RandomForestRegressor()
+model = xgb.XGBRegressor()
+values = cross_val_score(model, X_train, y_train, scoring='neg_mean_squared_log_error', cv=5)
+print(values)
+print(values.mean())
 model.fit(X_train, y_train)
 pred = model.predict(X_test)
 salida = pd.DataFrame({'Id': test_ids, 'SalePrice': pred})
-salida.to_csv("../data/resultados.csv", index=False)
+salida.to_csv("../data/resultados6.csv", index=False)
 
 
 
